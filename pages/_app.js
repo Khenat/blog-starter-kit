247a527dbc028a4502bdfc346a179d5267cb217e
Khenat <shubhaam.khenat@gmail.com>
2022-07-24 13:04:27 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,6 @@ import Head from 'next/head'
 
 import '../styles/main.css'
 
-import { annotate, annotationGroup } from 'rough-notation';
-
-const a1 = annotate(document.querySelector('#e1'), { type: 'underline' });
-const a2 = annotate(document.querySelector('#e3'), { type: 'box' });
-const a3 = annotate(document.querySelector('#e3'), { type: 'circle' });
-
-const ag = annotationGroup([a3, a1, a2]);
-ag.show();
-
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
@@ -29,7 +20,6 @@ export default function Nextra({ Component, pageProps }) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        <script async src="https://cdn.splitbee.io/sb.js"></script>
       </Head>
       <Component {...pageProps} />
     </>
